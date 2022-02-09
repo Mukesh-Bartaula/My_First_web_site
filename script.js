899f2f9d8e664400daf99d2bbd8240d7a7d875d3
Mukesh Bartaula <mukesh.bartaula109@gmail.com>
2022-02-09 19:11:24 +0545

    Feat: Password can't be empty

--- script.js
+++ script.js
@@ -4,12 +4,15 @@
 function checkValidation() {
 	let password = document.getElementById("password").value;
 	 let passwordSize = password.length;
-
-	if(passwordSize <= 6){
-
-		var passwordErrorElement = document.getElementById("password-error")
-		passwordErrorElement.innerHTML = "Password must be atleast 6 character."
+	 var passwordErrorElement = document.getElementById("password-error")
+	 let errorMessage = ""
+	if(!password){
+		errorMessage = "Password cann't be empty."
+	 } else if (passwordSize <= 6) {
+		
+		 errorMessage = "Password must be atleast 6 character."
 	}
+	passwordErrorElement.innerHTML = errorMessage
 }
 function showPassword(){
  var x = document.getElementById("password");
